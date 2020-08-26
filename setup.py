162087645a7dc657762f055ad77d5f5b0178266e
shawn <shawn.mccarthy@mongodb.com>
2020-08-26 00:03:45 -0400

    simple demo

--- setup.py
+++ setup.py
@@ -1,12 +1,22 @@
 """
 setup util
 """
-from setuptools import setup
+from setuptools import setup, find_packages
 
-setup(
-    name='pythoughtspot',
+setup_args = dict(
+    name='thoughtspot',
     version='1.0',
     description="demo packaging",
+    packages=find_packages(),
     url="https://github.com/shawnbmccarthy/periscope",
     data_files=[('config', ['cfg/thoughtspot.json'])]
 )
+
+install_requires = [
+    'pycrypto==2.6.1',
+    'requests==2.24.0'
+]
+
+if __name__ == '__main__':
+    setup(**setup_args, install_requires=install_requires, include_package_data=True)
+
