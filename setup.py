e805f2644caaa62d93b01f541d662a00af160856
shawn <shawn.mccarthy@mongodb.com>
2020-08-25 23:52:15 -0400

    simple demo

--- setup.py
+++ setup.py
@@ -4,6 +4,9 @@ setup util
 from setuptools import setup
 
 setup(
-    name='thoughtspot',
-    version='1.0'
+    name='pythoughtspot',
+    version='1.0',
+    description="demo packaging",
+    url="https://github.com/shawnbmccarthy/periscope",
+    data_files=[('config', ['cfg/thoughtspot.json'])]
 )
